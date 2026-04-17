ceff81b26dbc56d0675e39c0f06e5bacb6905baa
midulkhan <mnrkhn15@gmail.com>
2026-04-18 04:27:05 +0600

    projects

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,6 @@ import About from "./sections/About"
 import SkillsSlider from "./sections/SkillsSlider"
 import Project from "./sections/Project"
 
-
-
-
-
-
 export default function Home() {
   return (
     <div className="h-auto w-full">
