2e64b54ea2175122c9354759e83b834b60a6cf85
midulkhan <mnrkhn15@gmail.com>
2026-04-18 04:22:07 +0600

    project sections

--- app/WaterWave.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const WaterWave = dynamic(() => import("react-water-wave"), {
-  ssr: false,
-});
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,19 @@
-import Image from "next/image";
 import Hero from "./sections/Hero"
 import About from "./sections/About"
 import SkillsSlider from "./sections/SkillsSlider"
 import Project from "./sections/Project"
-import {WaterWave} from 'react-water-wave';
+
+
+
+
 
 
 export default function Home() {
   return (
     <div className="h-auto w-full">
-      
-        <WaterWave
-    imageUrl={"/midul.png"}
-  >
-    {methods => (
-     <Hero/>
 
-    )}
-  </WaterWave>
 
-      
+      <Hero/>
       <SkillsSlider/>
       <About/>
       <Project/>
